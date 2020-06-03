5c09bfc18cb915c7f9d94dfbd66dfb142e6a7dd3
RyanJeong <wat6988@gmail.com>
2020-06-03 12:03:18 +0900

    Updated on May 3 2020

--- fmt/print_test.go
+++ fmt/print_test.go
@@ -5,11 +5,18 @@ import "fmt"
 func ExampleFunctions_print() {
 	list1 := [...]int{1, 2, 3}
 	list2 := [...]int{4, 5, 6}
+	str1 := "STR1"
+	str2 := "STR2"
 
 	fmt.Println(list1, list2)
+	fmt.Println(str1, str2)
 	fmt.Print(list1, list2)
+	fmt.Print(str1, str2)
 	fmt.Printf("%d%d", list1, list2)
+	fmt.Printf("%s%s", str1, str2)
 	fmt.Printf("%d %d", list1, list2)
+	fmt.Printf("%s %s", str1, str2)
+
 	// Output:
 	// [1 2 3] [4 5 6]
 	// [1 2 3] [4 5 6][1 2 3][4 5 6][1 2 3] [4 5 6]
